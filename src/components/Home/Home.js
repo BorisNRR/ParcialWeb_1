0a0974ba6f929fc5903d04e67a24f1a11dab79e1
BorisNRR <b.reyesr@uniandes.edu.co>
2024-03-17 22:56:10 -0500

    Corregí el renderizado de las imagenes del grid y del perfil del usuario para facilitar comprensión en el último release

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.css"
 
 export function Home(){
     
@@ -23,14 +24,83 @@ export function Home(){
         fetch(URL).then(data => data.json()).then(data => {
             setUserData(data.results)
         })},[])
-        
+
+    const getImageGrid = () => {
+        if (window.innerWidth > 800) {
+          const images = [];
+          for (let i = 0; i < 12; i++) {
+            images.push(<Col key={i}><img src={"https://picsum.photos/350"} alt="post Photo"></img></Col>);
+          }
+          return (
+            <Row>{images}</Row>
+          );
+        } else if (window.innerWidth > 500) {
+          const images = [];
+          for (let i = 0; i < 12; i++) {
+            images.push(<Col key={i}><img src={"https://picsum.photos/350"} alt="post Photo"></img></Col>);
+          }
+          return (
+            <Row>
+              <Col>{images[0]}</Col>
+              <Col>{images[1]}</Col>
+              <Col>{images[2]}</Col>
+              <Col>{images[3]}</Col>
+              <Row>
+                <Col>{images[4]}</Col>
+                <Col>{images[5]}</Col>
+                <Col>{images[6]}</Col>
+              </Row>
+              <Row>
+                <Col>{images[7]}</Col>
+                <Col>{images[8]}</Col>
+                <Col>{images[9]}</Col>
+              </Row>
+              <Row>
+                <Col>{images[10]}</Col>
+                <Col>{images[11]}</Col>
+              </Row>
+            </Row>
+          );
+        } else {
+          const images = [];
+          for (let i = 0; i < 12; i++) {
+            images.push(<Col key={i}><img src={"https://picsum.photos/350"} alt="post Photo"></img></Col>);
+          }
+          return (
+            <Row>
+              <Col>{images[0]}</Col>
+              <Col>{images[1]}</Col>
+              <Row>
+                <Col>{images[2]}</Col>
+                <Col>{images[3]}</Col>
+              </Row>
+              <Row>
+                <Col>{images[4]}</Col>
+                <Col>{images[5]}</Col>
+              </Row>
+              <Row>
+                <Col>{images[6]}</Col>
+                <Col>{images[7]}</Col>
+              </Row>
+              <Row>
+                <Col>{images[8]}</Col>
+                <Col>{images[9]}</Col>
+              </Row>
+              <Row>
+                <Col>{images[10]}</Col>
+                <Col>{images[11]}</Col>
+              </Row>
+            </Row>
+          );
+        }
+      }
 
     return(
     <div className="container">
         <Row>
             <Col>
                 <a onClick={navigateToUserPRofile}>
-                    <img src={"https://picsum.photos/350"} alt="Profile Photo"></img>
+                    <img className="profilePhoto" width="150" height="150" src={"https://picsum.photos/350"} alt="Profile Photo"></img>
                 </a>
             </Col>
             <Col>
@@ -45,27 +115,7 @@ export function Home(){
             </Col>
         </Row>
         <Row>
-            <Row>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-            </Row>
-            <Row>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-            </Row>
-            <Row>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-            </Row>
-            <Row>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-                <Col><img src={"https://picsum.photos/350"} alt="post Photo" ></img></Col>
-            </Row>
-            
+            {getImageGrid()}
         </Row>
     </div>
     )
