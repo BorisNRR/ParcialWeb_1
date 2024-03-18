a54edb73fd122f70d8eca98f61c755dba20d752e
BorisNRR <b.reyesr@uniandes.edu.co>
2024-03-17 23:58:14 -0500

    Se añade internacionalización Inglés

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import { IntlProvider } from 'react-intl';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import localeEsMessages from "./locales/es"
+import localeEsMessages from "./locales/es";
+import localeEnMessages from './locales/en';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <IntlProvider locale="es-Es" messages={localeEsMessages}>
+  <IntlProvider locale="en-US" messages={localeEnMessages}>
     <App/>
   </IntlProvider>, 
   document.getElementById("root")
